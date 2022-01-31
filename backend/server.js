421d08a969796be9c372adb8fd3dac01a79a3cd5
nelson73 <nelson.concha.canto@gmail.com>
2022-01-31 11:17:57 -0600

    Enable cors in backend

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,22 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
+// Enable CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  );
+  res.header('Access-Control-Expose-Headers', 'Content-Count');
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  return next();
+});
+
 app.use("/api", routes);
 app.use("/*", (req, res, next) => Controller.notFound({ res }));
 
